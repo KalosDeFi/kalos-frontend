bdf7320fe23486af62f31f9e26dedc95bfcaccea
Kalos Protocol <kaizendex@protonmail.com>
2022-12-21 03:40:19 +1100

    Update index.tsx

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -101,18 +101,7 @@ const Home: React.FC = () => {
         </OuterWedgeWrapper>
         <SalesSection {...earnSectionData} />
       </PageSection>
-      <PageSection
-        innerProps={{ style: HomeSectionContainerStyles }}
-        background={
-          theme.isDark
-            ? 'linear-gradient(180deg, #0B4576 0%, #091115 100%)'
-            : 'linear-gradient(180deg, #6FB6F1 0%, #EAF2F6 100%)'
-        }
-        index={2}
-        hasCurvedDivider={false}
-      >
-        
-      </PageSection>
+      
       <PageSection
         innerProps={{ style: HomeSectionContainerStyles }}
         background={theme.colors.background}
