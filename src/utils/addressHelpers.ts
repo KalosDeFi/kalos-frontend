87982611568bbaf38dd43ba96ffd70dcf36a8f25
Kalos Protocol <kaizendex@protonmail.com>
2023-01-09 18:13:25 +1100

    address

--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -11,7 +11,7 @@ export const getAddress = (address: Address): string => {
 export const getCakeAddress = () => {
   return getAddress(tokens.cake.address)
 }
-export const getKaloAddress = () => {
+export const getXaloAddress = () => {
   return getAddress(tokens.xalo.address)
 }
 export const getMasterChefAddress = () => {
