8bc7e766f7b92a57ada999e9158db4287da09d13
Kalos Protocol <kaizendex@protonmail.com>
2023-01-13 18:41:16 +1100

    farms

--- src/__tests__/config/farms.test.ts
+++ src/__tests__/config/farms.test.ts
@@ -48,12 +48,12 @@ describe('Config farms', () => {
   })
 
   // The first pid using the new factory
-  const START_PID = 365
+  const START_PID = 5
   const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
   const newFarmsToTest = farmsToTest.filter((farmSet) => farmSet[0] >= START_PID)
 
   it.each(newFarmsToTest)('farm %d is using correct factory address', async (pid, farm) => {
-    const lpContract = getLpContract(farm.lpAddresses[56])
+    const lpContract = getLpContract(farm.lpAddresses[5])
     const factory = await lpContract.factory()
     expect(factory.toLowerCase()).toEqual(FACTORY_ADDRESS)
   })
